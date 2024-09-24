bfbbb4fab6dad7bbfcc90b9a2ef0eddb0bc39d20
ckw82076 <ckw82076@HA-STEMCART-33>
2024-09-24 10:07:51 -0400

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// A simple C++ Program
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Assuming the ocean's level is currently displayed and studies shows it is rising about 1.5 millimeters per year.\nDown below shows what the level will be at Year 5, Year 7, and Year 10.\n" << endl;
+	
+	cout << "By year 5, the number of millimeters risen from the current sea level will be 7.5\n" << endl;
+		
+	cout << "By year 7, the number of millimeters risen from the current sea level will be 10.0\n" << endl;
+
+	cout << "By year 10, the number of millimeters risen from the curent sea level will be 15.0\n" << endl;
+	return 0;
+}
